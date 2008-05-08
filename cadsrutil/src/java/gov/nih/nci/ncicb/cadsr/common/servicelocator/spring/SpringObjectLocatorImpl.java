5aac8343e9920d489f20d4f3274df39eb1a3f2bd
davet <davet@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-05-08 19:44:44 +0000

    4.0 caDSR-api changes
    SVN-Revision: 50

--- cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/servicelocator/spring/SpringObjectLocatorImpl.java
+++ cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/servicelocator/spring/SpringObjectLocatorImpl.java
@@ -1,25 +1,24 @@
 package gov.nih.nci.ncicb.cadsr.common.servicelocator.spring;
 import gov.nih.nci.ncicb.cadsr.common.servicelocator.ObjectLocator;
 import gov.nih.nci.ncicb.cadsr.common.servicelocator.ServiceLocatorException;
-import gov.nih.nci.ncicb.cadsr.spring.ApplicationContextFactory;
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
 
 
 public class SpringObjectLocatorImpl implements ObjectLocator
 
 {
-  public static ApplicationContext applicationContext = null;
-  public SpringObjectLocatorImpl()
-  {
-  }
-  
-  public Object findObject(String key)
-  {
-System.out.println("findobject " + key);
-	if(applicationContext==null)
-      throw new ServiceLocatorException("applicationContext is null");
-     return applicationContext.getBean(key);
-  }
-  
+	public static ApplicationContext applicationContext = null;
+	public SpringObjectLocatorImpl()
+	{
+	}
+
+	public Object findObject(String key)
+	{
+		System.out.println("findobject " + key);
+		if(applicationContext==null)
+			throw new ServiceLocatorException("applicationContext is null");
+		return applicationContext.getBean(key);
+	}
+
 }
