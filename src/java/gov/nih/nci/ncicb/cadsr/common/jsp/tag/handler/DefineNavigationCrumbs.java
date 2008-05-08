fba220f74112cab2bbc2545ba4aa397b71d05fbe
davet <davet@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-05-08 19:34:45 +0000

    4.0 caDSR-api changes
    SVN-Revision: 48

--- src/java/gov/nih/nci/ncicb/cadsr/common/jsp/tag/handler/DefineNavigationCrumbs.java
+++ src/java/gov/nih/nci/ncicb/cadsr/common/jsp/tag/handler/DefineNavigationCrumbs.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.ncicb.cadsr.common.jsp.tag.handler;
-import gov.nih.nci.ncicb.cadsr.domain.ObjectClass;
-import gov.nih.nci.ncicb.cadsr.domain.ObjectClassRelationship;
+import gov.nih.nci.cadsr.domain.ObjectClass;
+import gov.nih.nci.cadsr.domain.ObjectClassRelationship;
 import gov.nih.nci.ncicb.cadsr.common.jsp.bean.OCRNavigationBean;
 import gov.nih.nci.ncicb.cadsr.common.ocbrowser.struts.common.OCBrowserFormConstants;
 import java.io.IOException;
@@ -13,101 +13,101 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 public class DefineNavigationCrumbs extends TagSupport
 {
-  private String ocrId;
-  private String direction = null;
-  private String beanId = null;
+	private String ocrId;
+	private String direction = null;
+	private String beanId = null;
 
-  public DefineNavigationCrumbs()
-  {
-  }
+	public DefineNavigationCrumbs()
+	{
+	}
 
-  public int doStartTag() throws javax.servlet.jsp.JspException {
-    HttpServletRequest  req;
-    JspWriter out;
+	public int doStartTag() throws javax.servlet.jsp.JspException {
+		HttpServletRequest  req;
+		JspWriter out;
 
-    try
-    {
-      out = pageContext.getOut();
-      req = ( HttpServletRequest )pageContext.getRequest();
+		try
+		{
+			out = pageContext.getOut();
+			req = ( HttpServletRequest )pageContext.getRequest();
 
-      LinkedList newCrumbs = new LinkedList();
-      pageContext.getAttribute(ocrId);
+			LinkedList newCrumbs = new LinkedList();
+			pageContext.getAttribute(ocrId);
 
-      OCRNavigationBean firstNavBean = new OCRNavigationBean();
-      firstNavBean.setDirection(direction);
-      firstNavBean.setShowDirection(true);
-      ObjectClassRelationship ocr = (ObjectClassRelationship)pageContext.getAttribute(ocrId);
-      firstNavBean.setOcr(ocr);
-      ObjectClass currObject= (ObjectClass)req.getSession().getAttribute(OCBrowserFormConstants.OBJECT_CLASS);
-      firstNavBean.setObjectClass(currObject);
-      newCrumbs.add(firstNavBean);
+			OCRNavigationBean firstNavBean = new OCRNavigationBean();
+			firstNavBean.setDirection(direction);
+			firstNavBean.setShowDirection(true);
+			ObjectClassRelationship ocr = (ObjectClassRelationship)pageContext.getAttribute(ocrId);
+			firstNavBean.setOcr(ocr);
+			ObjectClass currObject= (ObjectClass)req.getSession().getAttribute(OCBrowserFormConstants.OBJECT_CLASS);
+			firstNavBean.setObjectClass(currObject);
+			newCrumbs.add(firstNavBean);
 
-      OCRNavigationBean secondNavBean = new OCRNavigationBean();
-      secondNavBean.setShowDirection(false);
-      ObjectClass otherObject= null;
-       if(direction.equals(OCBrowserFormConstants.OUT_GOING_OCRS))
-         otherObject =  ocr.getTarget();
-       if(direction.equals(OCBrowserFormConstants.IN_COMMING_OCRS))
-         otherObject =  ocr.getSource();
-       if(direction.equals(OCBrowserFormConstants.BIDIRECTIONAL_OCRS))
-       {
-         if(ocr.getTarget().getId().equals(currObject.getId()))
-         {
-           otherObject=ocr.getSource();
-         }
-         else
-         {
-           otherObject=ocr.getTarget();
-         }
-       }
-       secondNavBean.setObjectClass(otherObject);
-       newCrumbs.add(secondNavBean);
+			OCRNavigationBean secondNavBean = new OCRNavigationBean();
+			secondNavBean.setShowDirection(false);
+			ObjectClass otherObject= null;
+			if(direction.equals(OCBrowserFormConstants.OUT_GOING_OCRS))
+				otherObject =  ocr.getTargetObjectClass();
+			if(direction.equals(OCBrowserFormConstants.IN_COMMING_OCRS))
+				otherObject =  ocr.getSourceObjectClass();
+			if(direction.equals(OCBrowserFormConstants.BIDIRECTIONAL_OCRS))
+			{
+				if(ocr.getTargetObjectClass().getId().equals(currObject.getId()))
+				{
+					otherObject=ocr.getSourceObjectClass();
+				}
+				else
+				{
+					otherObject=ocr.getTargetObjectClass();
+				}
+			}
+			secondNavBean.setObjectClass(otherObject);
+			newCrumbs.add(secondNavBean);
 
-       pageContext.setAttribute(beanId,newCrumbs);
+			pageContext.setAttribute(beanId,newCrumbs);
 
-    }
-    catch (Exception e)
-    {
-      throw new JspException( "I/O Error : " + e.getMessage() );
-    }
-    return Tag.SKIP_BODY;
-  }
-
-
-  public void setOcrId(String ocrId)
-  {
-    this.ocrId = ocrId;
-  }
+		}
+		catch (Exception e)
+		{
+			throw new JspException( "I/O Error : " + e.getMessage() );
+		}
+		return Tag.SKIP_BODY;
+	}
 
 
-  public String getOcrId()
-  {
-    return ocrId;
-  }
+	public void setOcrId(String ocrId)
+	{
+		this.ocrId = ocrId;
+	}
 
 
-  public void setDirection(String direction)
-  {
-    this.direction = direction;
-  }
+	public String getOcrId()
+	{
+		return ocrId;
+	}
 
 
-  public String getDirection()
-  {
-    return direction;
-  }
+	public void setDirection(String direction)
+	{
+		this.direction = direction;
+	}
 
 
-  public void setBeanId(String beanId)
-  {
-    this.beanId = beanId;
-  }
+	public String getDirection()
+	{
+		return direction;
+	}
 
 
-  public String getBeanId()
-  {
-    return beanId;
-  }
+	public void setBeanId(String beanId)
+	{
+		this.beanId = beanId;
+	}
+
+
+	public String getBeanId()
+	{
+		return beanId;
+	}
 
 
 }
