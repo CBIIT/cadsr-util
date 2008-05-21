7a9234ca8d74a1343a4493a4b07aec086d19ef68
hegdes <hegdes@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-05-21 19:30:50 +0000

    moved the cart classes to objectCart package
    SVN-Revision: 52

---
+++ src/java/gov/nih/nci/ncicb/cadsr/objectCart/CDECart.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.ncicb.cadsr.objectCart;
+
+
+import java.util.Collection;
+
+public interface CDECart  {
+  public Collection getDataElements();
+  public void setDataElements(Collection items);
+  public void setDataElement(CDECartItem item);
+
+  public Collection getForms();
+  public void setForm(CDECartItem form);
+  public void setForms(Collection forms);
+
+  public void removeDataElement(String itemId);
+  public void removeDataElements(Collection items);
+
+  public CDECartItem findDataElement(String itemId);
+
+  public void mergeCart(CDECart cart);
+}
