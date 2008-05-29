5eb5413dab91f5a3f2833c8b4252f552b3d35549
davet <davet@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-05-29 19:39:30 +0000

    Implemented 4.0 changes for VM as ACs and CSI as ACs
    SVN-Revision: 68

--- src/java/gov/nih/nci/ncicb/cadsr/common/cdebrowser/service/CDEBrowserService.java
+++ src/java/gov/nih/nci/ncicb/cadsr/common/cdebrowser/service/CDEBrowserService.java
@@ -1,9 +1,10 @@
 package gov.nih.nci.ncicb.cadsr.common.cdebrowser.service;
 
-import java.util.List;
-
 import gov.nih.nci.ncicb.cadsr.common.resource.DataElement;
+import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -13,5 +14,5 @@ public interface CDEBrowserService
 	public Properties reloadApplicationProperties(Locale locale, String username);
 	public void populateDataElementAltNameDef (DataElement de);
 	public List getReferenceDocuments(String acIdseq);
-	//public List getReferenceDocumentsForCSI(String cscsiIdseq);
+	//public List getReferenceDocumentsForCSI(String cscsiIdseq);	
 }
