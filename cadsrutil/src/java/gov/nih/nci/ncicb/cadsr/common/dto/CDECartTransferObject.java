e77fd492670b9321dc3d1e798269afd3ef3f592a
davet <davet@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-06-02 20:13:51 +0000

    CDE Cart Implementation of Object Cart
    SVN-Revision: 74

--- cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/dto/CDECartTransferObject.java
+++ cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/dto/CDECartTransferObject.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -84,6 +85,14 @@ public class CDECartTransferObject implements CDECart, Serializable {
 
   public void mergeDataElements(Collection items){
 	  //TODO 
+  }
+  
+  public void expireCart(){
+		//TODO  
+  }
+	  
+  public void expireCart(Date expireDate){
+		  //TODO
   }
   
 }
