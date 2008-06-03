c3111378eac9745bd96aeb6d2d4b70d4eb3eb320
hegdes <hegdes@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-06-03 15:53:08 +0000

    get formbuilder property from tool options
    SVN-Revision: 75

--- src/java/gov/nih/nci/ncicb/cadsr/common/cdebrowser/service/CDEBrowserService.java
+++ src/java/gov/nih/nci/ncicb/cadsr/common/cdebrowser/service/CDEBrowserService.java
@@ -1,10 +1,9 @@
 package gov.nih.nci.ncicb.cadsr.common.cdebrowser.service;
 
-import gov.nih.nci.ncicb.cadsr.common.resource.DataElement;
-import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
-
-import java.util.Collection;
 import java.util.List;
+
+import gov.nih.nci.ncicb.cadsr.common.resource.DataElement;
+
 import java.util.Locale;
 import java.util.Properties;
 
@@ -14,5 +13,6 @@ public interface CDEBrowserService
 	public Properties reloadApplicationProperties(Locale locale, String username);
 	public void populateDataElementAltNameDef (DataElement de);
 	public List getReferenceDocuments(String acIdseq);
-	//public List getReferenceDocumentsForCSI(String cscsiIdseq);	
+	//public List getReferenceDocumentsForCSI(String cscsiIdseq);
+	public Properties getApplicationProperties(Locale locale, String toolName);
 }
