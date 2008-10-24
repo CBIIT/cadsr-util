92280ed49a7a1c07d48b27e67f1caf3feb52b342
(no author) <(no author)@f9237d49-2742-4819-83f0-f984798bf4f4>
2008-10-24 20:59:49 +0000

    This commit was manufactured by cvs2svn to create tag 'CDEBrowser_4_0_0_0'.
    SVN-Revision: 109

---
+++ cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/dto/CDECartTransferObject.java
@@ -0,0 +1,98 @@
+package gov.nih.nci.ncicb.cadsr.common.dto;
+
+import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
+import gov.nih.nci.ncicb.cadsr.objectCart.CDECartItem;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class CDECartTransferObject implements CDECart, Serializable {
+  protected Set dataElements;
+  protected Set forms;
+
+  public CDECartTransferObject() {
+    dataElements = new HashSet();
+    forms = new HashSet();
+  }
+
+  public Collection getDataElements() {
+    return dataElements;
+  }
+
+  public void setDataElements(Collection items) {
+    dataElements.addAll(items);
+  }
+
+  public Collection getForms() {
+    return forms;
+  }
+
+  public void setForm(CDECartItem form) {
+    forms.add(form);
+  }
+
+  public void setDataElement(CDECartItem item) {
+    dataElements.add(item);
+  }
+
+  public void setForms(Collection forms) {
+    forms.addAll(forms);
+  }
+
+  public void mergeCart(CDECart cart) {
+    Collection deColl = cart.getDataElements();
+    this.dataElements.addAll(deColl);
+    this.forms.addAll(cart.getForms());
+  }
+
+  public void removeDataElement(String itemId) {
+  }
+
+  public void removeDataElements(Collection items) {
+  }
+
+  public CDECartItem findDataElement(String itemId) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    CDECartTransferObject c = new CDECartTransferObject();
+    Collection l = new ArrayList();
+    CDECartItem i1 = new CDECartItemTransferObject();
+    CDECartItem i2 = new CDECartItemTransferObject();
+
+    i1.setId("2");
+    i2.setId("1");
+    l.add(i1);
+    l.add(i2);
+
+    c.setDataElements(l);
+
+    System.out.println("Cart size: " + c.getDataElements().size());
+    System.out.println("Hashcode of i1: " + i1.hashCode());
+    System.out.println("Hashcode of i2: " + i2.hashCode());
+  }
+
+  public void associateCart(String userId) {
+	// TODO Auto-generated method stub	
+  }
+
+  public void mergeDataElements(Collection items){
+	  //TODO 
+  }
+  
+  public void expireCart(){
+		//TODO  
+  }
+	  
+  public void expireCart(Date expireDate){
+		  //TODO
+  }
+  
+}
