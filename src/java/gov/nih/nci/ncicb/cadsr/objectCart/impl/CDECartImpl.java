418242607bcc4c519643dc881bf5dd1c25ac3a31
avdicd <avdicd@f9237d49-2742-4819-83f0-f984798bf4f4>
2010-04-19 18:28:21 +0000

    SVN-Revision: 127

--- src/java/gov/nih/nci/ncicb/cadsr/objectCart/impl/CDECartImpl.java
+++ src/java/gov/nih/nci/ncicb/cadsr/objectCart/impl/CDECartImpl.java
@@ -98,4 +98,14 @@ public class CDECartImpl implements CDECart, Serializable  {
   public void expireCart(Date expireDate){
 	  //TODO
   }
+
+public String getCartId() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public String getCartName() {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
