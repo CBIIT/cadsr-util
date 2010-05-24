d5a95b94a62a6e53fdaa650a86cc9ec121689e63
mathura2 <mathura2@f9237d49-2742-4819-83f0-f984798bf4f4>
2010-05-24 17:36:43 +0000

    initial revision
    SVN-Revision: 137

---
+++ src/java/gov/nih/nci/ncicb/cadsr/common/resource/ComponentType.java
@@ -0,0 +1,59 @@
+package gov.nih.nci.ncicb.cadsr.common.resource;
+
+public enum ComponentType {
+	
+	CDE,
+	DEC,
+	VD,
+	OC,
+	PROP,
+	CONCEPT,
+	REPRESENTATION,
+	CONCEPTUAL_DOMAIN,
+	CS,
+	CSI,
+	FORMELEMENT,
+	UNKNOWN;
+	
+	public static ComponentType getComponentType(String _value) {
+		if (_value.equalsIgnoreCase("QUEST_CONTENT")) {
+			return FORMELEMENT;
+		} 
+		else if (_value.equalsIgnoreCase("DATAELEMENT")) {
+			return CDE;
+		}
+		else if (_value.equalsIgnoreCase("DE_CONCEPT")) {
+			return DEC;
+		}
+		else if (_value.equalsIgnoreCase("VALUEDOMAIN")) {
+			return VD;
+		}
+		else if (_value.equalsIgnoreCase("OBJECTCLASS")) {
+			return OC;
+		}
+		else if (_value.equalsIgnoreCase("PROPERTY")) {
+			return PROP;
+		}
+		else if (_value.equalsIgnoreCase("CONCEPT")) {
+			return CONCEPT;
+		}
+		else if (_value.equalsIgnoreCase("CONCEPT")) {
+			return CONCEPT;
+		}
+		else if (_value.equalsIgnoreCase("CONCEPTUALDOMAIN")) {
+			return CONCEPTUAL_DOMAIN;
+		}
+		else if (_value.equalsIgnoreCase("REPRESENTATION")) {
+			return REPRESENTATION;
+		}
+		else if (_value.equalsIgnoreCase("CLASSIFICATION")) {
+			return CS;
+		}
+		else if (_value.equalsIgnoreCase("CS_ITEM")) {
+			return CSI;
+		}
+		else {
+			 return UNKNOWN;
+		}
+	}
+}
