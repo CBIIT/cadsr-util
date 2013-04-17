e488af0dfee779cdacfc0195b3de3aecccb650a7
liur3 <liur3@f9237d49-2742-4819-83f0-f984798bf4f4>
2013-04-17 21:38:01 +0000

    SVN-Revision: 162

--- cadsrutil/src/java/gov/nih/nci/ncicb/webtree/ContextNode.java
+++ cadsrutil/src/java/gov/nih/nci/ncicb/webtree/ContextNode.java
@@ -41,7 +41,7 @@ public class ContextNode extends LazyActionTreeNode {
                
             }
          
-         if (Context.CONTEXT_CABIG.equals(contextName)) {
+         if (Context.CONTEXT_NCI.equals(contextName)) {
             LazyActionTreeNode publishNode = new LazyActionTreeNode("Folder",
             "Catalogue of Published Forms", getIdentifier(), false);
             super.addLeaf(publishNode);
