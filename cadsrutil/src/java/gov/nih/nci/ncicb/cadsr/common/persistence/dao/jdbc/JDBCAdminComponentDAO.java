107ef89d9fe726d7b449ec1457e4e6d963c3fbc4
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-05-05 14:25:13 -0400

    Added date created and modified fields in get designation and definition queries

--- cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/persistence/dao/jdbc/JDBCAdminComponentDAO.java
+++ cadsrutil/src/java/gov/nih/nci/ncicb/cadsr/common/persistence/dao/jdbc/JDBCAdminComponentDAO.java
@@ -1425,7 +1425,8 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
       public void setSql(String type) {
        if (type== null){
         super.setSql(
-          "select des.desig_idseq, des.name, des.detl_name, des.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME "+
+          "select des.desig_idseq, des.name, des.detl_name, des.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME, "+
+        	" DES.DATE_CREATED, DES.DATE_MODIFIED " +
             " from sbr.contexts_view con, sbr.designations_view des "+
             " where des.CONTE_IDSEQ = con.CONTE_IDSEQ "+
             " and des.ac_idseq = ?");
@@ -1433,7 +1434,8 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
         declareParameter(new SqlParameter("AC_IDSEQ", Types.VARCHAR));
        }else{
            super.setSql(
-             "select des.desig_idseq, des.name, des.detl_name, des.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME "+
+             "select des.desig_idseq, des.name, des.detl_name, des.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME, "+
+           " DES.DATE_CREATED, DES.DATE_MODIFIED " +
            " from sbr.contexts_view con, sbr.designations_view des "+
            " where des.CONTE_IDSEQ = con.CONTE_IDSEQ "+
            " and des.ac_idseq = ? and des.detl_name=?");
@@ -1467,6 +1469,9 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
         cto.setName(rs.getString(6));
         dto.setContext(cto);
 
+        dto.setDateCreated(rs.getTimestamp("DATE_CREATED"));
+        dto.setDateModified(rs.getTimestamp("DATE_MODIFIED"));
+        
         return dto;
       }
     }//end of private class
@@ -1480,7 +1485,7 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
       public void setSql() {
         super.setSql(
           " select def.defin_idseq, def.definition,  def.defl_name, " +
-          " def.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME " +
+          " def.lae_name, con.CONTE_IDSEQ, con.NAME, con.PAL_NAME, DEF.DATE_CREATED, DEF.DATE_MODIFIED " +
           " from sbr.contexts_view con, sbr.definitions_view def " +
           " where def.CONTE_IDSEQ = con.CONTE_IDSEQ " +
           " and def.ac_idseq = ? ");
@@ -1497,7 +1502,7 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
       protected Object mapRow(
         ResultSet rs,
         int rownum) throws SQLException {
-        Definition dto = new DefinitionTransferObject();
+    	  DefinitionTransferObject dto = new DefinitionTransferObject();
         //fixing
         dto.setId(rs.getString(1));
         dto.setDefinition(rs.getString(2));
@@ -1508,6 +1513,9 @@ public class JDBCAdminComponentDAO extends JDBCBaseDAO
         cto.setConteIdseq(rs.getString(5));
         cto.setName(rs.getString(6));
         dto.setContext(cto);
+        
+        dto.setDateCreated(rs.getTimestamp("DATE_CREATED"));
+        dto.setDateModified(rs.getTimestamp("DATE_MODIFIED"));
 
         return dto;
       }
